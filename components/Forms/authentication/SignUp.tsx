be97fdb84c2f9f87bee655ae5bc53226a9f29d6a
ridhoajibx <ridhoajibx@gmail.com>
2023-05-08 16:42:21 +0700

    [ridho] - custom css react-phone input - WIP

--- components/Forms/authentication/SignUp.tsx
+++ components/Forms/authentication/SignUp.tsx
@@ -1,9 +1,9 @@
 import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
-import { MdEmail, MdLockOutline, MdOutlineEmail, MdOutlinePerson } from 'react-icons/md'
+import { MdEmail, MdLockOutline, MdOutlineCalendarToday, MdOutlineEmail, MdOutlinePerson } from 'react-icons/md'
 import Button from '../../Button/Button'
-import DropdownSelectPopout from '../../Dropdown/DropdownSelectPopout'
 import DropdownSelect from '../../Dropdown/DropdownSelect'
+import PhoneInput from 'react-phone-input-2';
 
 type Props = {
     onChangePage: () => void
@@ -70,6 +70,7 @@ const stylesSelect = {
 const SignUp = (props: Props) => {
     const { onChangePage, isOpen } = props;
     const [gender, setGender] = useState(null);
+    const [phone, setPhone] = useState("");
 
     return (
         <div className={`absolute bg-white right-0 top-0 z-50 flex w-full lg:w-1/2 h-full flex-col overflow-hidden duration-1000 ease-linear ${isOpen ? 'translate-y-0 lg:translate-x-0' : 'translate-y-full lg:translate-x-full opacity-0'}`}>
@@ -128,6 +129,46 @@ const SignUp = (props: Props) => {
                         </div>
                     </div>
 
+                    <div className="w-full flex flex-col lg:flex-row mb-0 lg:mb-3 gap-2">
+                        <div className='w-full lg:w-1/2 mb-3 lg:mb-0'>
+                            <label htmlFor='name' className='mb-2.5 block font-medium text-black dark:text-white'>
+                                Phone Number *
+                            </label>
+                            <div className='text-gray-5'>
+                                <PhoneInput
+                                    specialLabel=''
+                                    country={"id"}
+                                    value={phone}
+                                    onChange={setPhone}
+                                    buttonClass='shadow-default'
+                                    placeholder='1 123 4567 8910'
+                                    inputClass='form-control py-4 px-6 border border-stroke focus:border-primary rounded-lg text-sm lg:text-md'
+                                    dropdownClass='right-0'
+                                    searchClass='focus:border-primary selected:border-primary'
+                                    containerClass='flex flex-row-reverse'
+                                />
+                            </div>
+                        </div>
+
+                        <div className='w-full lg:w-1/2 mb-3 lg:mb-0'>
+                            <label htmlFor='date' className='mb-2.5 block font-medium text-black dark:text-white'>
+                                Date of birth *
+                            </label>
+                            <div className='relative'>
+                                <input
+                                    id='date'
+                                    type='date'
+                                    placeholder='Date of birth'
+                                    className='text-sm lg:text-md w-full text-gray-5 rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary'
+                                />
+
+                                <label htmlFor="date">
+                                    <MdOutlineCalendarToday className='absolute right-4 top-4 h-6 w-6 text-gray-5' />
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+
                     <div className='mb-3'>
                         <label htmlFor='email' className='mb-2.5 block font-medium text-black dark:text-white'>
                             Email *
@@ -149,7 +190,7 @@ const SignUp = (props: Props) => {
                         </label>
                         <div className='relative'>
                             <input
-                                id="email"
+                                id="password"
                                 type='password'
                                 placeholder='Enter your password'
                                 className='w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary'
