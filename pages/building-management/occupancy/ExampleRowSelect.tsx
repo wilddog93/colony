cb2f3cef4c7a0f95d8b9aa219620652032f8f9fd
ridhoajibx <ridhoajibx@gmail.com>
2023-05-25 09:18:38 +0700

    [ridho] - change sidebar component - WIP

--- pages/building-management/occupancy/ExampleRowSelect.tsx
+++ pages/building-management/occupancy/ExampleRowSelect.tsx
@@ -15,6 +15,8 @@ import { ColumnDef } from '@tanstack/react-table';
 import { ColumnItems, makeData } from '../../../components/tables/components/makeData';
 import { formatPhone } from '../../../utils/useHooks/useFunction';
 import { IndeterminateCheckbox } from '../../../components/tables/components/TableComponent';
+import SidebarComponent from '../../../components/Layouts/Sidebar/SidebarComponent';
+import { menuBM } from '../../../utils/routes';
 
 const stylesSelect = {
   indicatorsContainer: (provided: any) => ({
@@ -271,7 +273,12 @@ const Occupancy = (props: any) => {
       description=""
     >
       <div className='absolute inset-0 mt-20 z-9 bg-boxdark flex text-white'>
-        <SidebarBM sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+        <SidebarComponent
+          className=''
+          menus={menuBM}
+          sidebar={sidebarOpen}
+          setSidebar={setSidebarOpen}
+        />
 
         <div className="relative w-full bg-white lg:rounded-tl-[3rem] p-8 pt-0 2xl:p-10 2xl:pt-0 overflow-y-auto">
           <div className='sticky bg-white top-0 z-50 w-full flex flex-col lg:flex-row items-start lg:items-center justify-between py-6 mb-3 gap-2'>
